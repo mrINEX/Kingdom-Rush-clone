87714cc3b408efe6c3c8aba76a0c7515be732632
mrinex <mrinex@mail.ru>
2021-02-05 00:18:57 +0300

    docs: add Kingdom Rush clone

--- frontend/src/scripts/components/Gate.ts
+++
@@ -1,7 +0,0 @@
-export default class Gate extends Phaser.GameObjects.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y, texture);
-    scene.add.existing(this);
-    this.setInteractive();
-  }
-}
